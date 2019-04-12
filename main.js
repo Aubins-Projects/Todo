b08fcb2b65d197b50dd386dfcc617549bdc00a8d
simplycodingthings <aubinh92@gmail.com>
2019-04-11 21:24:41 -0400

    Rework using JSON stringify

--- main.js
+++ main.js
@@ -1,18 +1,37 @@
-// JavaScript source code
-function test() {
-    console.log("test worked")
+//Global Variable
+var ListOfTasks = Array();
 
+//JSON Function
+
+function SaveListToJSON(list) {
+    localStorage.dict = JSON.stringify(list);
 }
 
-var dict = {
-    Test1: Array(1, 2, 30, false),
-    Test2: Array(2, 1, 30, false),
-    Test3: Array(4, 0, 30, false),
-    Test4: Array(3, 3, 0, false)
+function ImportBackToList(myJSON) {
+    return JSON.parse(myJSON);
 }
 
+//Default Array for Tasks
 
-function addTaks() {
+function setDefaultList(num) {
+    var temp_list = [];
+    for (var i = 0; i < num; i++) {
+        temp_list.push(Array(("Test" + i), Array(i, i, i,"12-April-2019", "false")));
+    }
+    return temp_list;
+}
+//Due Date Functions
+var monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"];
+function dateFunction() {
+    var today = new Date();
+    var date = prompt("Please enter due date.", today.getDate() + "-" + monthNames[today.getMonth()] + "-" + today.getFullYear());
+    return date
+}   
+
+//Add new Tasks to the List
+
+function addTask() {
     var task = prompt("What is the task?", "Type Here");
     if (task == null || task == "") {
         console.log("Don't try and break it");
@@ -23,50 +42,44 @@ function addTaks() {
         } else {
             var minutes = prompt("What is the time to complete the task in minutes?", "Type Here");
             if (minutes == null || minutes == "") {
-                //console.log("Don't try and break it");
+                console.log("Don't try and break it");
             } else {
                 var priority = prompt("What is the priority?", "Type Here");
                 if (priority == null || priority == "") {
-                    //console.log("Don't try and break it");
+                    console.log("Don't try and break it");
                 } else {
-                    dict[task] = priority +"," + time + "," + minutes + "," + "false";
-                   // console.log(localStorage.dict);
-                   // console.log("end of adding");
                     
-                    listMaker();
-                    
-                   // console.log(localStorage.dict);
-                   // console.log("end of load list");
+                    ListOfTasks.push(Array(task, Array(priority, time, minutes, dateFunction(),"false")));
+                    SaveListToJSON(ListOfTasks);
+                    tablizer(ListOfTasks);
                 }
             }
         }
     }
 }
 
-function clickCounter() {
-    if (typeof (Storage) !== "undefined") {
-        if (localStorage.clickcount) {
-            localStorage.clickcount = Number(localStorage.clickcount) + 1;
-        } else {
-            localStorage.clickcount = 1;
-        }
-        document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
-    } else {
-        document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
-    }
+
+// JavaScript source code
+function test() {
+    console.log("test worked")
+
 }
+
 function listMaker() {
     if (typeof (Storage) !== "undefined") {
         
         var new_string_list = "";
         var print_string = ('<table style="width: 100%"><tr><th>Number</th><th>Task</th><th>Priority</th><th>Hours</th><th>Minutes</th></tr>');
         var counter = 1;
-        for (var key in dict) {
+        for (var i = 0; i < ListOfTasks.length; i++) {
+            //console.log(dict);
             print_string += "<tr>";
-            new_string_list += key + " " + dict[key] + "<br>";
-            var temporary_list = fix_string_list(dict[key]);
-            console.log(temporary_list[2]);
-            print_string += "<td>" + counter + "</td><td>" + key + "</td><td>" + temporary_list[0] + "</td><td>" + temporary_list[1] + "</td><td>" + temporary_list[2] + "</td>";
+            new_string_list += ListOfTasks[i][0] + " " + ListOfTasks[i][1] + "<br>";
+            console.log("made it here");
+            //if (dict[key]='undefined')
+            
+            
+            print_string += "<td>" + counter + "</td><td>" + ListOfTasks[i][0] + "</td><td>" + ListOfTasks[i][1][0] + "</td><td>" + ListOfTasks[i][1][1] + "</td><td>" + ListOfTasks[i][1][2] + "</td>";
             counter++;
             print_string += "</tr>";
         }
@@ -87,13 +100,13 @@ function remover() {
         txt = "User cancelled the prompt.";
     } else {
         var counter = 1;
-        for (var prop in dict) {
-            console.log(counter);
-            console.log(item);
+        for (var i = 0; i < ListOfTasks.length;i++) {
+            //console.log(counter);
+            console.log(ListOfTasks[i]);
             console.log(counter == parseInt(item, 10));
             if (counter == parseInt(item, 10)) {
                 console.log("removing the item");
-                delete dict[prop];
+                delete ListOfTasks[i];
             }
             counter += 1;
         }
@@ -102,72 +115,88 @@ function remover() {
 }
 
 function loadList() {
-    var temp_list2 = [];
+    var temp_list2 = Array();
     var temp_list = localStorage.dict.split('<br>');
-    //console.log(temp_list);
-    make_dict(printList(temp_list));
+    console.log("This is the loadlist output for breaking on br");
+    console.log(temp_list);
+    console.log("This is the end of the loadlist output for breaking on br");
+    for (var i = 0; i < temp_list.length-1; i++) {
+        temp_list2.push( temp_list[i].split('! '));
+    }
+    console.log(temp_list2);
+    ListOfTasks = temp_list2;
+    //make_dict(printList(temp_list));
     
 }
 
 function printList(t_list) {
     var new_list = [];
-    for (var i = 0; i < t_list.length; i++) {
+    for (var i = 0; i < t_list.length-1; i++) {
         //console.log(t_list[i]);
-        var temp_item = t_list[i].split(' ');
-        //console.log(temp_item);
+        var temp_item = t_list[i].split('! ');
+        console.log(temp_item);
         new_list.push(temp_item);
     }
+    console.log(new_list);
     return new_list;
 }
 
-function make_dict(l) {
-    dict = {};
-    for (var i = 0; i < l.length-1; i++) {
-        if (l.length > 0) {
-            dict[l[i][0]] = l[i][1];
-        }
-    }
-    //console.log(dict)
-}
+
 
 function fix_string_list(strings) {
-    return strings.split(',');
+    if (typeof (strings) == 'string') {
+        return strings.split(',');
+    }
+    else {
+        return strings;
+    }
 }
 
 function priority() {
     var order_list = [];
-    for (var item in dict) {
-        order_list.push(Array(item, parseInt(fix_string_list(dict[item])[0], 10), parseInt(fix_string_list(dict[item])[1], 10), parseInt(fix_string_list(dict[item])[2],10)));
+    for (var i = 0; i < ListOfTasks.length; i++) {
+        order_list.push(Array(ListOfTasks[i][0], Array(parseInt(ListOfTasks[i][1][0], 10), parseInt(ListOfTasks[i][1][1], 10), parseInt(ListOfTasks[i][1][2],10))));
             }
     order_list = order_list.sort(Comparator);
-    //console.log(order_list);
+    console.log("This is where to look");
+    console.log(ListOfTasks);
+    console.log(order_list);
     tablizer(order_list);
+    AdditAllUp(order_list);
 }
 
 function Comparator(a, b) {
-    if (a[1] < b[1]) return -1;
-    if (a[1] > b[1]) return 1;
+    if (a[1][0] < b[1][0]) return -1;
+    if (a[1][0] > b[1][0]) return 1;
     return 0;
+}
+
+function AdditAllUp(list) {
+    var totalminutes = 0;
+    for (var i = 0; i < list.length; i++) {
+        totalminutes += list[i][1][2];
+        totalminutes += list[i][1][1] * 60;
+        console.log(totalminutes);
+
+    }
 }
 
 function tablizer(list) {
     if (typeof (Storage) !== "undefined") {
-
-       
         var print_string = ('<table style="width: 100%"><tr><th>Task</th><th>Priority</th><th>Hours</th><th>Minutes</th></tr>');
-       
-        for (var i = 0; i < list.length - 1; i++) {
+        for (var i = 0; i < list.length; i++) {
             print_string += "<tr>";
-            print_string += "<td>" + list[i][0] + "</td><td>" + list[i][1] + "</td><td>" + list[i][2] + "</td><td>" + list[i][3] + "</td>";
+            print_string += "<td>" + list[i][0] + "</td><td>" + list[i][1][0] + "</td><td>" + list[i][1][1] + "</td><td>" + list[i][1][2] + "</td>";
             
             print_string += "</tr>";
         }
         print_string += "</table >";
-        
-
         document.getElementById("list").innerHTML = print_string;
-
     } else {
         document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
     }
+}
+
+function clearLocalStorage() {
+    localStorage.dict = {};
 }
